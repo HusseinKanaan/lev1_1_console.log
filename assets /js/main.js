cf36b4761b2378faae07ac45960cb9f3adbe12b7
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-07-15 00:21:18 +0200

    first commit

---
+++ assets /js/main.js
@@ -0,0 +1,28 @@
+a = 12;
+b = a * 3;
+console.log(b);
+
+// document.write(b)
+
+
+console.log(document.URL);
+
+// Aufgabe: Variable Deklarieren
+
+
+console.log("Hello World!")
+
+let firstname = 'Anton'
+console.log(firstname)
+
+let age = 28
+console.log(age)
+
+let job = 'Trainer'
+console.log(job)
+
+let married= 'verheiratet'
+married = true
+console.log (married)
+
+console.log(firstname + ' ist '+ age + ' Jahre alt, vom Beruf ' + job +' und ist ' + married  )
